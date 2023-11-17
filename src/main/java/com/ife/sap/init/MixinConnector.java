7e3a72fa625d9aee0f76c2ba936d5d818ca4d0d6
neighborsbear <dkfowlqrhdxo@naver.com>
2023-11-17 18:37:07 +0900

    실패다 연금술사!

---
+++ src/main/java/com/ife/sap/init/MixinConnector.java
@@ -0,0 +1,11 @@
+package com.ife.sap.init;
+
+import org.spongepowered.asm.mixin.Mixins;
+import org.spongepowered.asm.mixin.connect.IMixinConnector;
+
+public class MixinConnector implements IMixinConnector {
+
+    @Override
+    public void connect() {
+        Mixins.addConfiguration("mixin.sap.json");}
+}
