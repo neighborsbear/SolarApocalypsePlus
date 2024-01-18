7edacfbff06434d01060e8c350f4a1a791cc0a43
gisellevonbingen <gisellevonbingen@gmail.com>
2024-01-18 20:55:18 +0900

    Update BlockBehaviourMixin.java

--- src/main/java/com/ife/sap/mixin/BlockBehaviourMixin.java
+++ src/main/java/com/ife/sap/mixin/BlockBehaviourMixin.java
@@ -5,6 +5,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.ife.sap.BlockBehaviourAccessor;
 import com.ife.sap.SapMod;
@@ -22,13 +25,21 @@ public abstract class BlockBehaviourMixin implements BlockBehaviourAccessor {
     protected boolean isRandomlyTicking;
 
     @Unique
+    private boolean sap$originalIsRandomlyTicking;
+
+    @Unique
     protected Procedure sap$procedure;
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void init(BlockBehaviour.Properties properties, CallbackInfo callbackInfo) {
+        this.sap$originalIsRandomlyTicking = this.isRandomlyTicking;
+    }
 
     @Override
     public void sap$initCaches() {
         Block block = (Block) (Object) this;
         this.sap$procedure = SapMod.getProcedure(block);
-        this.isRandomlyTicking = this.sap$procedure != null;
+        this.isRandomlyTicking = this.sap$originalIsRandomlyTicking || this.sap$procedure != null;
     }
 
     @Override
